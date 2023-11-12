bc4aea4c9b89851dbdae883dd2a212e7da6268bf
markhong0605 <hung0986079811@gmail.com>
2023-11-12 23:30:17 +0800

    hi

---
+++ python_file_pie_chart01/data.js
@@ -0,0 +1,98 @@
+let evaluation_ratio = [
+    {
+        "name" : "平時",
+        "count" : 10
+    },
+    {
+        "name" : "出席",
+        "count" : 20
+    },
+    {
+        "name" : "期中評量",
+        "count" : 30
+    },
+    {
+        "name" : "期末評量",
+        "count" : 40
+    }
+];
+
+let evaluation_ratio_02 = [
+    {
+        "name" : "平時",
+        "count" : 20
+    },
+    {
+        "name" : "出席",
+        "count" : 10
+    },
+    {
+        "name" : "期中評量",
+        "count" : 35
+    },
+    {
+        "name" : "期末評量",
+        "count" : 35
+    }
+];
+
+let evaluation_ratio_03 = [
+    {
+        "name" : "平時",
+        "count" : 10
+    },
+    {
+        "name" : "出席",
+        "count" : 30
+    },
+    {
+        "name" : "期中評量",
+        "count" : 20
+    },
+    {
+        "name" : "期末評量",
+        "count" : 40
+    }
+];
+
+let evaluation_ratio_04 = [
+    {
+        "name" : "平時",
+        "count" : 10
+    },
+    {
+        "name" : "出席",
+        "count" : 10
+    },
+    {
+        "name" : "期中評量",
+        "count" : 30
+    },
+    {
+        "name" : "期末評量",
+        "count" : 50
+    }
+];
+
+let evaluation_ratio_05 = [
+    {
+        "name" : "洪正豪",
+        "count" : 100-16-6-21-13
+    },
+    {
+        "name" : "Mark",
+        "count" : 16
+    },
+    {
+        "name" : "Mark Hong",
+        "count" : 6
+    },
+    {
+        "name" : "馬克洪",
+        "count" : 21
+    },
+    {
+        "name" : "馬克洪敢",
+        "count" : 13
+    }
+];
